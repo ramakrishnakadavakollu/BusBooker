eba8745365adef9c08752cd907e8157a45f160a7
ramaKrishnaKadavakollu <ksr.krishna01@gmail.com>
2023-08-29 17:37:52 +0530

    modifued

--- Busbookes/src/main/java/com/rk/bb/entity/User.java
+++ Busbookes/src/main/java/com/rk/bb/entity/User.java
@@ -1,5 +1,7 @@
 package com.rk.bb.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
+@Getter
 @Table(name = "user_table", uniqueConstraints = { @UniqueConstraint(columnNames = "username"),
 		@UniqueConstraint(columnNames = "email") })
 public class User {
